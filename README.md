d89b529988061683490783b0fc851ef0e9a60a9e
PRATAP PANABAKA <pratap@fastmail.com>
2024-09-08 19:50:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React + Vite + TailwindCSS Template
+
+Example:
+```
+gh repo create myrepo_name --template https://github.com/PRATAP-KUMAR/pratap-panabaka-react-vite-tailwind-template --private  --clone
+```
---
+++ src/pages/Details.jsx
@@ -0,0 +1,11 @@
+function Details() {
+    return (
+        <div className="bg-toolite">
+            <div className='max-width center-div bg-lite'>
+                <h3 className='font-bold font-custom'>Details</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Details
